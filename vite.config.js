93fbdd98b4f848e4e49ca4ec0d7ccab372a0334e
yapupiales <yapupiales42@soy.sena.edu.co>
2026-01-22 19:55:24 -0500

    Fix produccion (#34)
    * Fix produccion
    
    ---------
    
    Co-authored-by: Jeffer
    Co-authored-by: DriveLoop

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        watch: {
+            ignored: [
+                '**/node_modules/**',
+                '**/vendor/**',
+                '**/storage/**',
+            ]
+        }
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/validar_fecha_busqueda.js'],
