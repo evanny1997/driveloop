53b3aa917bd2d82ba2ed1cf40427c8a550bb11ea
evanny1997 <evanni.08@hotmail.com>
2026-02-25 20:04:06 -0500

    feat: Implement digital payment processing using Mercado Pago and contract generation/confirmation.

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn() => view('home'));
 
+Route::get('/contrato/descargar/{id}', [ContratoController::class, 'generar'])->name('contrato.descargar');
+
 require __DIR__ . '/breeze/routes.php';
 require __DIR__ . '/breeze/auth.php';
 
 foreach (glob(app_path('Modules/*/routes.php')) as $route) {
     require $route;
 }
-
-
